e9679389ebcd2dfc3c0602ca5bb5ebdad86bcfde
David Torras <davidtorrasceron@gmail.com>
2022-11-17 11:39:29 +0100

    Add tests for new features

--- tests/Feature/Read/GetTranslationsDtoActionTest.php
+++ tests/Feature/Read/GetTranslationsDtoActionTest.php
@@ -2,8 +2,9 @@
 
 use UseLocale\LocaleLaravel\DTOs\ApiTranslationsDto;
 use UseLocale\LocaleLaravel\Read\GetTranslationsDtoAction;
+use UseLocale\LocaleLaravel\Read\ScannerService;
 
-test('dto have got translations', function () {
+test('dto structure is correct', function () {
     /** @var ApiTranslationsDto $apiTranslationsDto */
     $apiTranslationsDto = resolve(GetTranslationsDtoAction::class)('en');
 
@@ -12,36 +13,43 @@ test('dto have got translations', function () {
         ->toHaveProperty('locale')
         ->toHaveProperty('jsonData')
         ->toHaveProperty('phpData');
+});
 
+test('dto have got translations', function () {
+    /** @var ApiTranslationsDto $apiTranslationsDto */
+    $apiTranslationsDto = resolve(GetTranslationsDtoAction::class)('en');
     expect($apiTranslationsDto->locale)->toBe('en');
 
     expect($apiTranslationsDto->jsonData)
         ->toBeArray()
-        ->toHaveKey('foo', 'bar');
+        ->toBe([
+            'foo' => 'bar',
+            'breeze' => 'alpine',
+            'JSON trans' => null,
+            'JSON trans_choice' => null,
+            'JSON Lang::get' => null,
+            'JSON Lang::choice' => null,
+            'JSON Lang::trans' => null,
+            'JSON Lang::transChoice' => null,
+            'JSON @lang' => null,
+            'JSON @choice' => null,
+            'JSON __' => null,
+        ]);
 
     expect($apiTranslationsDto->phpData)
         ->toBeArray()
-        ->toHaveKey('norf' . DIRECTORY_SEPARATOR . 'fubar.passport', 'horizon')
-        ->toHaveKey('quux.lorem', 'ipsum');
-});
-
-test("dto havent got translations", function () {
-    /** @var ApiTranslationsDto $apiTranslationsDto */
-    $apiTranslationsDto = resolve(GetTranslationsDtoAction::class)('missingLocale');
-
-    expect($apiTranslationsDto)
-        ->toBeInstanceOf(ApiTranslationsDto::class)
-        ->toHaveProperty('locale')
-        ->toHaveProperty('jsonData')
-        ->toHaveProperty('phpData');
-
-    expect($apiTranslationsDto->locale)->toBe('missingLocale');
-
-    expect($apiTranslationsDto->jsonData)
-        ->toBeArray()
-        ->toBeEmpty();
-
-    expect($apiTranslationsDto->phpData)
-        ->toBeArray()
-        ->toBeEmpty();
+        ->toBe([
+            'norf' . DIRECTORY_SEPARATOR . 'fubar.passport' => 'horizon',
+            'quux.lorem' => 'ipsum',
+            'quux.taylor' => 'otwell',
+            'php.trans' => null,
+            'php.trans_choice' => null,
+            'php.Lang::get' => null,
+            'php.Lang::choice' => null,
+            'php.Lang::trans' => null,
+            'php.Lang::transChoice' => null,
+            'php.@lang' => null,
+            'php.@choice' => null,
+            'php.__' => null,
+        ]);
 });
